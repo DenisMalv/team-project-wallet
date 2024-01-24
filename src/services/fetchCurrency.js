a5343b6dc43ca0ad5f69ca783e6a57ef3b872e2d
Denis Malniev <kissed03@gmail.com>
2024-01-24 13:17:27 +0200

    Update fetchCurrency.js

--- src/services/fetchCurrency.js
+++ src/services/fetchCurrency.js
@@ -1,7 +1,8 @@
 const fetchCurrency = async () => {
 	try {
 		const response = await fetch(
-			"https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+			// "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+			"https://wallet-api-5ezy.onrender.com/api/currensies"
 		);
 		const rates = response.json();
 		// console.log(rates);
